6d33b9b21c50e72242ac8a9e9165ea896b95d447
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-03-29 20:15:25 +0530

    Build

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,32 @@ const App = () => {
   return (
     <div id="main">
       <div class="container">
-                <div class="community">
-                  <h2 class="community-header">Join our community</h2>
-                </div>
-                <div class="card-details">
-                  <div class="card">
-                    <p class="card-header">Monthly Subscription</p>
-                  </div>
-                  <div class="why-us">
-                    <h4 class="why-us-header">Why Us</h4>
-                  </div>
-                </div>
-              </div>
+        <div class="community">
+          <h2 class="community-header">Join our community</h2>
+          <h2>30-day hassle-free money back guarantee</h2>
+          <h3>
+            Gain access to our library of tutorials along with expert code
+            reviews. Perfect for any developer who wants to learn.
+          </h3>
+        </div>
+        <div class="card-details">
+          <div class="card">
+            <p class="card-header">Monthly Subscription</p>
+            <span>$29</span>
+            <span>Per Month</span>
+            <div>Full access for less than $1 a day.</div>
+            <button>Sign Up</button>
+          </div>
+          <div class="why-us">
+            <h4 class="why-us-header">Why Us</h4>
+            <div>
+              Tutorials by industry experts Peer & expert code review Coding
+              exercises Access yo our GitHub repos Community forum Flashcard
+              decks
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
